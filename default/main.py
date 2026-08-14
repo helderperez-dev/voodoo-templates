77d694d32087f5eb24bd0b61f15435ea9dbca714
Helder Perez <helder.perez@avynta.com>
2026-08-14 20:36:24 -0300

    Update templates to use h11 parser and increase max incomplete event size to fix cookie headers limit

--- default/main.py
+++ default/main.py
@@ -5,4 +5,13 @@ from voodoo.config import config
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=config.host, port=config.port, reload=True, ws_max_size=16777216, ws_max_queue=32)
+    uvicorn.run(
+        "main:app", 
+        host=config.host, 
+        port=config.port, 
+        reload=True, 
+        ws_max_size=16777216, 
+        ws_max_queue=32,
+        http="h11",
+        h11_max_incomplete_event_size=5242880
+    )
